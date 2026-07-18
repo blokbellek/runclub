6113d91d92aae0b96c3b5d86c627ecff3032103f
blokbellek <surbims@gmail.com>
2026-07-18 15:33:47 +0300

    Update footer: Powered by Savaş

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-[rgb(230,230,230)] bg-[rgb(245,245,245)] py-10 px-8 text-center">
       <p className="text-sm leading-[16.8px] text-[rgb(81,81,81)] m-0">
-        Copyright © {currentYear} Cappadocia Run Club | Powered by Gutenic
+        Copyright © {currentYear} Cappadocia Run Club | Powered by Savaş
       </p>
     </footer>
   );
